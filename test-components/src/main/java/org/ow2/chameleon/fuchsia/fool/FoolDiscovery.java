b662343eb74b2851368aa98e1c631f915ff7cf46
Morgan Martinet <morgan.martinet@imag.fr>
2013-07-12 12:52:22 +0200

    Update fool-components

--- test-components/src/main/java/org/ow2/chameleon/fuchsia/fool/FoolDiscovery.java
+++ test-components/src/main/java/org/ow2/chameleon/fuchsia/fool/FoolDiscovery.java
@@ -2,12 +2,14 @@ package org.ow2.chameleon.fuchsia.fool;
 
 import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Instantiate;
-import org.apache.felix.ipojo.annotations.Provides;
+import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Validate;
 import org.osgi.framework.BundleContext;
 import org.ow2.chameleon.fuchsia.core.component.AbstractDiscoveryComponent;
-import org.ow2.chameleon.fuchsia.core.component.DiscoveryService;
 import org.ow2.chameleon.fuchsia.core.declaration.ImportDeclaration;
 import org.ow2.chameleon.fuchsia.core.declaration.ImportDeclarationBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,10 +24,8 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
  * It don't really get what it does, but it's ok.
  */
 @Component(name = "Fuchsia-FoolDiscovery-Factory")
-@Provides(specifications={DiscoveryService.class})
 @Instantiate(name = "Fuchsia-FoolDiscovery")
 public class FoolDiscovery extends AbstractDiscoveryComponent {
-
 
     private final ScheduledThreadPoolExecutor pool_register = new ScheduledThreadPoolExecutor(1);
     private final ScheduledThreadPoolExecutor pool_unregister = new ScheduledThreadPoolExecutor(1);
@@ -34,14 +34,20 @@ public class FoolDiscovery extends AbstractDiscoveryComponent {
     private Integer index_importDeclaration_register;
     private Integer index_importDeclaration_unregister;
 
+    /**
+     * logger
+     */
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public FoolDiscovery(BundleContext bundleContext) {
         super(bundleContext);
-        System.err.println("Creating fool discovery !");
+        logger.debug("Creating fool discovery !");
     }
 
-
+    @Validate
     public void start() {
-        System.err.println("Start fool discovery !");
+        super.start();
+        logger.debug("Start fool discovery !");
         for (int i = 0; i < 2; i++) {
             Map<String, Object> metadata = new HashMap<String, Object>();
             metadata.put("fool", "fool");
@@ -51,14 +57,16 @@ public class FoolDiscovery extends AbstractDiscoveryComponent {
         index_importDeclaration_register = -1;
         index_importDeclaration_unregister = -1;
 
-//        pool_register.execute(new Registrator());
-//        pool_unregister.execute(new Unregistrator());
+        pool_register.execute(new Registrator());
+        pool_unregister.execute(new Unregistrator());
 
     }
 
+    @Invalidate
     public void stop() {
-//        pool_unregister.shutdown();
-//        pool_register.shutdown();
+        super.stop();
+        pool_unregister.shutdown();
+        pool_register.shutdown();
 
         importDeclarations.clear();
     }
@@ -68,14 +76,16 @@ public class FoolDiscovery extends AbstractDiscoveryComponent {
 
         Random random = new Random();
 
+        private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
         public void run() {
             while (index_importDeclaration_register < importDeclarations.size()) {
                 try {
-                    Thread.sleep((random.nextInt(5) + 1) * 10000);
+                    Thread.sleep((random.nextInt(5) + 1) * 3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
+                logger.debug("Registrator : " + index_importDeclaration_register);
                 registerImportDeclaration(importDeclarations.get(index_importDeclaration_register + 1));
                 index_importDeclaration_register = index_importDeclaration_register + 1;
             }
@@ -87,15 +97,18 @@ public class FoolDiscovery extends AbstractDiscoveryComponent {
 
         Random random = new Random();
 
+        private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
         public void run() {
             while (index_importDeclaration_unregister < importDeclarations.size()) {
                 while (index_importDeclaration_unregister == index_importDeclaration_register) {
                     try {
-                        Thread.sleep((random.nextInt(5) + 1) * 1000);
+                        Thread.sleep((random.nextInt(5) + 1) * 2000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
+                logger.debug("Unregistrator : " + index_importDeclaration_unregister);
 
                 unregisterImportDeclaration(importDeclarations.get(index_importDeclaration_unregister + 1));
                 index_importDeclaration_unregister = index_importDeclaration_unregister + 1;
