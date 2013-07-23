7268cae6624333f3ee39728aed5d4f9bd70a27cd
Morgan Martinet <morgan.martinet@imag.fr>
2013-07-23 17:25:07 +0200

    Complete and enable FoolImporter component

--- fool-components/src/main/java/org/ow2/chameleon/fuchsia/fool/FoolImporter.java
+++ fool-components/src/main/java/org/ow2/chameleon/fuchsia/fool/FoolImporter.java
@@ -1,23 +1,59 @@
 package org.ow2.chameleon.fuchsia.fool;
 
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Provides;
+import org.apache.felix.ipojo.annotations.ServiceProperty;
+import org.apache.felix.ipojo.annotations.Validate;
 import org.ow2.chameleon.fuchsia.core.component.AbstractImporterComponent;
+import org.ow2.chameleon.fuchsia.core.component.ImporterService;
 import org.ow2.chameleon.fuchsia.core.declaration.ImportDeclaration;
+import org.ow2.chameleon.fuchsia.core.util.BadImportRegistration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
-
+@Component(name = "Fuchsia-FoolImporter-Factory")
+@Provides(specifications = ImporterService.class)
+@Instantiate(name = "Fuchsia-FoolImporter")
 public class FoolImporter extends AbstractImporterComponent {
+    /**
+     * logger
+     */
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @ServiceProperty(name = "target", value = "(fool-number=1)")
+    private String filter;
+
+    @Override
+    @Validate
+    protected void stop() {
+        super.stop();
+    }
+
+    @Override
+    @Invalidate
+    protected void start() {
+        super.start();
+    }
+
     @Override
     protected void createProxy(ImportDeclaration importDeclaration) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        logger.debug("FoolImporter create a proxy for " + importDeclaration);
     }
 
     @Override
     protected void destroyProxy(ImportDeclaration importDeclaration) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        logger.debug("FoolImporter destroy a proxy for " + importDeclaration);
     }
 
     public List<String> getConfigPrefix() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        List<String> l = new ArrayList<String>();
+        l.add("fool");
+        return l;
     }
 }
