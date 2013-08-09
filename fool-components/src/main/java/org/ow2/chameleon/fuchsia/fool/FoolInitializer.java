99cebde9e38bfd9d87e19db67f6bbbd7c6b2b907
Morgan Martinet <morgan.martinet@imag.fr>
2013-08-09 16:29:48 +0200

    Remove FuchsiaMediator*

--- fool-components/src/main/java/org/ow2/chameleon/fuchsia/fool/FoolInitializer.java
+++ fool-components/src/main/java/org/ow2/chameleon/fuchsia/fool/FoolInitializer.java
@@ -1,39 +1,17 @@
 package org.ow2.chameleon.fuchsia.fool;
 
-import org.apache.felix.ipojo.ConfigurationException;
-import org.apache.felix.ipojo.MissingHandlerException;
-import org.apache.felix.ipojo.UnacceptableConfiguration;
-import org.apache.felix.ipojo.annotations.Bind;
-import org.apache.felix.ipojo.annotations.Component;
-import org.apache.felix.ipojo.annotations.Instantiate;
-import org.apache.felix.ipojo.annotations.Unbind;
-import org.osgi.framework.InvalidSyntaxException;
-import org.ow2.chameleon.fuchsia.core.FuchsiaMediator;
+import org.apache.felix.ipojo.configuration.Configuration;
+import org.apache.felix.ipojo.configuration.Instance;
 
-@Component(name = "Fuchsia-FoolInitializer-Factory")
-@Instantiate(name = "Fuchsia-FoolInitializer")
+import static org.apache.felix.ipojo.configuration.Instance.instance;
+import static org.ow2.chameleon.fuchsia.core.Linker.PROPERTY_FILTER_IMPORTDECLARATION;
+
+@Configuration
 public class FoolInitializer {
 
-    @Bind(aggregate = false, id = "fm")
-    public void bindFuchsiaMediator(FuchsiaMediator fm) {
-        try {
-            fm.createLinker("FoolLinker").importDeclarationFilter("(*=*)").build();
-        } catch (InvalidSyntaxException e) {
-            e.printStackTrace();
-        } catch (MissingHandlerException e) {
-            e.printStackTrace();
-        } catch (ConfigurationException e) {
-            e.printStackTrace();
-        } catch (UnacceptableConfiguration e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    @Unbind(id = "fm")
-    public void unbindFuchsiaMediator(FuchsiaMediator fm) {
-        fm.destroyLinker("FoolLinker");
-
-    }
+    Instance roseMachineInstance = instance()
+            .of("FuchsiaDefaultLinkerFactory")
+            .named("FoolLinker")
+            .with(PROPERTY_FILTER_IMPORTDECLARATION).setto("(fool=fool)");
 
 }
