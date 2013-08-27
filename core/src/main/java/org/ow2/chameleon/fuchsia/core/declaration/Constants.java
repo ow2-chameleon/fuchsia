abe551240dff19774715ac1616b13302fc14da50
Morgan Martinet <morgan.martinet@imag.fr>
2013-08-27 09:06:01 +0200

    Add a declaration.Constants class
    The declaration.Constants class will regroup all the keys employed to configure
    the Declarations (ImportDeclarations & ExportDeclarations)

---
+++ core/src/main/java/org/ow2/chameleon/fuchsia/core/declaration/Constants.java
@@ -0,0 +1,9 @@
+package org.ow2.chameleon.fuchsia.core.declaration;
+
+
+public interface Constants {
+    String URL = "url";
+    String ID = "id";
+    String SERVICE_CLASS = "service.class";
+    String CONFIGS = "configs";
+}
