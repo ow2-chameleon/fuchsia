8274f6ccb927ec57f961af913df658f0562fd32e
Morgan Martinet <morgan.martinet@imag.fr>
2013-08-28 13:06:18 +0200

    Fix DiscoveryTest in fake-it module

--- fake/fake-it/src/test/java/org/ow2/chameleon/fuchsia/fake/tests/DiscoveryTest.java
+++ fake/fake-it/src/test/java/org/ow2/chameleon/fuchsia/fake/tests/DiscoveryTest.java
@@ -1,30 +1,21 @@
 package org.ow2.chameleon.fuchsia.fake.tests;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.ops4j.pax.exam.Option;
-import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceReference;
 import org.ow2.chameleon.fuchsia.fake.device.GenericDevice;
-import org.ow2.chameleon.fuchsia.fake.device.GenericFakeDevice;
 import org.ow2.chameleon.fuchsia.testing.CommonTest;
-
-import javax.inject.Inject;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
 
 /**
- * Test class to test the weel functionning of discovery->fake device->proxy
+ * Test class to test the well functioning of discovery->fake device->proxy
  *
  * @author jeremy.savonet@gmail.com
  */
 public class DiscoveryTest extends CommonTest {
-
-    @Inject
-    private BundleContext m_bundleContext;
 
     /**
      * Common test options.
@@ -40,22 +31,6 @@ public class DiscoveryTest extends CommonTest {
         );
     }
 
-    /**
-     * Common test setup.
-     */
-    @Before
-    public void commonSetUp() {
-        super.commonSetUp();
-    }
-
-    /**
-     * Common test tear down.
-     */
-    @Override
-    public void commonTearDown() {
-        super.commonTearDown();
-    }
-
     @Override
     public boolean deployTestBundle() {
         return false;
@@ -64,41 +39,26 @@ public class DiscoveryTest extends CommonTest {
 
     @Test
     public void testCreateFakeBL() throws InvalidSyntaxException {
-
-        ServiceReference[] deviceRef = m_bundleContext.getServiceReferences(GenericDevice.class.getName(), "(device.serialNumber=BL-1234)");
-        Object device = m_bundleContext.getService(deviceRef[0]);
-        GenericFakeDevice genericDevice = (GenericFakeDevice) device;
-
+        GenericDevice genericDevice = osgiHelper.getServiceObject(GenericDevice.class, "(device.serialNumber=BL-1234)");
         assertThat(genericDevice.getSerialNumber()).isEqualTo("BL-1234");
     }
 
     @Test
     public void testCreateFakeDL() throws InvalidSyntaxException {
-
-        ServiceReference[] deviceRef = m_bundleContext.getServiceReferences(GenericDevice.class.getName(), "(device.serialNumber=DL-5678)");
-        Object device = m_bundleContext.getService(deviceRef[0]);
-        GenericFakeDevice genericDevice = (GenericFakeDevice) device;
-
+        GenericDevice genericDevice = osgiHelper.getServiceObject(GenericDevice.class, "(device.serialNumber=DL-5678)");
         assertThat(genericDevice.getSerialNumber()).isEqualTo("DL-5678");
     }
 
     @Test
     public void testCreateFakePT() throws InvalidSyntaxException {
-
-        ServiceReference[] deviceRef = m_bundleContext.getServiceReferences(GenericDevice.class.getName(), "(device.serialNumber=PT-1234)");
-        Object device = m_bundleContext.getService(deviceRef[0]);
-        GenericFakeDevice genericDevice = (GenericFakeDevice) device;
-
+        GenericDevice genericDevice = osgiHelper.getServiceObject(GenericDevice.class, "(device.serialNumber=PT-1234)");
         assertThat(genericDevice.getSerialNumber()).isEqualTo("PT-1234");
     }
 
 
 //    @Test
 //    public void testCreateProxyBL() throws InvalidSyntaxException {
-//
-//        ServiceReference[] deviceRef = m_bundleContext.getServiceReferences(GenericDevice.class.getName(),"(device.serialNumber=BL-1234)");
-//        Object device =  m_bundleContext.getService(deviceRef[0]);
-//        GenericFakeDevice genericDevice = (GenericFakeDevice) device;
+//        GenericDevice genericDevice = osgiHelper.getServiceObject(GenericDevice.class, "(device.serialNumber=BL-1234)");
 //
 //        GenericDevice proxy = (GenericDevice) Proxy.newProxyInstance(DelegationProxy.class.getClassLoader(), new Class[]{GenericDevice.class}, new DelegationProxy(genericDevice));
 //        assertThat(proxy.getSerialNumber()).isEqualTo("BL-1234");
