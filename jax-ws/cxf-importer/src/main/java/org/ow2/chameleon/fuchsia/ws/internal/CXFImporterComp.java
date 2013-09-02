2ed03c782c7f89f82342e99c1c08e8e297dd7905
Jeremy Savonet <jeremy.savonet@gmail.com>
2013-09-02 09:21:07 +0200

    Use Fuchsia Core loadClass in importer

--- jax-ws/cxf-importer/src/main/java/org/ow2/chameleon/fuchsia/ws/internal/CXFImporterComp.java
+++ jax-ws/cxf-importer/src/main/java/org/ow2/chameleon/fuchsia/ws/internal/CXFImporterComp.java
@@ -9,12 +9,13 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.ow2.chameleon.fuchsia.core.component.AbstractImporterComponent;
 import org.ow2.chameleon.fuchsia.core.declaration.ImportDeclaration;
-import org.ow2.chameleon.fuchsia.examples.jaxws.helloWS.HelloWorldWS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jws.WebService;
 import java.util.*;
+
+import static org.ow2.chameleon.fuchsia.core.FuchsiaUtils.loadClass;
 
 /**
  * This component provides a JAX-WS, Apache CXF based implementation of an
@@ -30,6 +31,7 @@ public class CXFImporterComp extends AbstractImporterComponent {
     public static final String ENDPOINT_URL = "endpoint.url"; //TODO FIXME
 
     public static final String INTERFACES = "jax-ws.importer.interfaces";
+    private static final String CLASSNAME = "className";
 
     private final Map<ImportDeclaration, ServiceRegistration> map;
 
@@ -90,18 +92,22 @@ public class CXFImporterComp extends AbstractImporterComponent {
         String endPointURL;
         Object objectProxy;
 
-        List<Class<?>> testClass = new ArrayList<Class<?>>();
-        testClass.add(HelloWorldWS.class);
         //Try to load the class
-        final List<Class<?>> klass = testClass;
-//        final List<Class<?>> klass = loadClasses(context, importDeclaration);
+//        final List<Class<?>> klass = testClass;
+        final Class<?> klass;
+        try {
+            klass = loadClass(context, (String) importDeclaration.getMetadata().get(CLASSNAME));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            return;
+        }
 
         final ClassLoader origin = Thread.currentThread().getContextClassLoader();
         try {
             Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
-            logger.debug(String.valueOf(klass.get(0)));
+            logger.debug(String.valueOf(klass));
             //use annotations if present
-            if (klass.get(0).isAnnotationPresent(WebService.class)){
+            if (klass.isAnnotationPresent(WebService.class)){
                 factory = new JaxWsProxyFactoryBean();
             } else {
                 factory = new ClientProxyFactoryBean();
@@ -111,7 +117,7 @@ public class CXFImporterComp extends AbstractImporterComponent {
             factory.getOutInterceptors().add(new LoggingOutInterceptor());
 
             //set the class XXX only one class is supported
-            factory.setServiceClass(klass.get(0));
+            factory.setServiceClass(klass);
 
             //set the URL
             if(!(importDeclaration.getMetadata().get(ENDPOINT_URL) instanceof String)) {
@@ -126,7 +132,7 @@ public class CXFImporterComp extends AbstractImporterComponent {
 
             //create the proxy
             objectProxy = factory.create();
-            HelloWorldWS newObject = (HelloWorldWS)objectProxy;
+//            HelloWorldWS newObject = (HelloWorldWS)objectProxy;
 
             //Publish object
             map.put(importDeclaration,registerProxy(objectProxy,klass));
@@ -139,10 +145,10 @@ public class CXFImporterComp extends AbstractImporterComponent {
      * Utility method to register a proxy has a Service in OSGi.
      *
      */
-    protected ServiceRegistration registerProxy(Object objectProxy, List<?> clazz) {
+    protected ServiceRegistration registerProxy(Object objectProxy, Class clazz) {
         Dictionary<String, Object> props = new Hashtable<String, Object>();
         ServiceRegistration registration;
-        registration = context.registerService((Class)clazz.get(0), objectProxy, props);
+        registration = context.registerService(clazz, objectProxy, props);
 
         return registration;
     }
