810590fc4064ed71f3b32c3922e59944fac969f0
Jeremy Savonet <jeremy.savonet@gmail.com>
2013-09-04 16:27:29 +0200

    Clean dead code in fake project

--- fake/fake-it/src/test/java/org/ow2/chameleon/fuchsia/fake/tests/DiscoveryTest.java
+++ fake/fake-it/src/test/java/org/ow2/chameleon/fuchsia/fake/tests/DiscoveryTest.java
@@ -13,6 +13,7 @@ import static org.ops4j.pax.exam.CoreOptions.options;
 /**
  * Test class to test the well functioning of discovery->fake device->proxy
  *
+ * Note :
  * @author jeremy.savonet@gmail.com
  */
 public class DiscoveryTest extends CommonTest {
@@ -35,33 +36,4 @@ public class DiscoveryTest extends CommonTest {
     public boolean deployTestBundle() {
         return false;
     }
-
-
-    @Test
-    public void testCreateFakeBL() throws InvalidSyntaxException {
-        GenericDevice genericDevice = osgiHelper.getServiceObject(GenericDevice.class, "(device.serialNumber=BL-1234)");
-        assertThat(genericDevice.getSerialNumber()).isEqualTo("BL-1234");
-    }
-
-    @Test
-    public void testCreateFakeDL() throws InvalidSyntaxException {
-        GenericDevice genericDevice = osgiHelper.getServiceObject(GenericDevice.class, "(device.serialNumber=DL-5678)");
-        assertThat(genericDevice.getSerialNumber()).isEqualTo("DL-5678");
-    }
-
-    @Test
-    public void testCreateFakePT() throws InvalidSyntaxException {
-        GenericDevice genericDevice = osgiHelper.getServiceObject(GenericDevice.class, "(device.serialNumber=PT-1234)");
-        assertThat(genericDevice.getSerialNumber()).isEqualTo("PT-1234");
-    }
-
-
-//    @Test
-//    public void testCreateProxyBL() throws InvalidSyntaxException {
-//        GenericDevice genericDevice = osgiHelper.getServiceObject(GenericDevice.class, "(device.serialNumber=BL-1234)");
-//
-//        GenericDevice proxy = (GenericDevice) Proxy.newProxyInstance(DelegationProxy.class.getClassLoader(), new Class[]{GenericDevice.class}, new DelegationProxy(genericDevice));
-//        assertThat(proxy.getSerialNumber()).isEqualTo("BL-1234");
-//
-//    }
 }
