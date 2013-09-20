569f5294436936ae65989e8cc73bbebed033e0ed
jbotnascimento <jbotnascimento@gmail.com>
2013-09-20 11:06:14 +0200

    add throwable parameter ImporterException constructor so its possible to re-throw the stack

--- core/src/main/java/org/ow2/chameleon/fuchsia/core/exceptions/ImporterException.java
+++ core/src/main/java/org/ow2/chameleon/fuchsia/core/exceptions/ImporterException.java
@@ -1,4 +1,12 @@
 package org.ow2.chameleon.fuchsia.core.exceptions;
 
 public class ImporterException extends Exception {
+
+    public ImporterException(Throwable cause) {
+        super(cause);
+    }
+
+    public ImporterException(String message) {
+        super(message);
+    }
 }
