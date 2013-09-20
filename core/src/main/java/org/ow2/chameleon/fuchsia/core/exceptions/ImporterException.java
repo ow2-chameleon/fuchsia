398dc4c129ce5f2c13be5698cc4f9f934dc0aab0
jbotnascimento <jbotnascimento@gmail.com>
2013-09-20 11:06:14 +0200

    [core] Add ImporterException constructors

--- core/src/main/java/org/ow2/chameleon/fuchsia/core/exceptions/ImporterException.java
+++ core/src/main/java/org/ow2/chameleon/fuchsia/core/exceptions/ImporterException.java
@@ -1,4 +1,16 @@
 package org.ow2.chameleon.fuchsia.core.exceptions;
 
 public class ImporterException extends Exception {
+
+    public ImporterException(Throwable cause) {
+        super(cause);
+    }
+
+    public ImporterException(String message) {
+        super(message);
+    }
+
+    public ImporterException(String message, Throwable e) {
+        super(message, e);
+    }
 }
