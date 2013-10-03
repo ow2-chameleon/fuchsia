a3b0275327e3cdbc1174e948a09deb0f044d0b4b
jbotnascimento <jbotnascimento@gmail.com>
2013-10-03 14:24:30 +0200

    add a mqtt-config example bundle
    initializes a linker and the importer for an mqtt service (based on rabbit)

---
+++ examples/mqtt-config/src/main/java/org/ow2/chameleon/fuchsia/push/examples/config/MqttLinkerInitializer.java
@@ -0,0 +1,25 @@
+package org.ow2.chameleon.fuchsia.push.examples.config;
+
+import org.apache.felix.ipojo.configuration.Configuration;
+import org.apache.felix.ipojo.configuration.Instance;
+import org.ow2.chameleon.fuchsia.core.FuchsiaConstants;
+
+import static org.apache.felix.ipojo.configuration.Instance.instance;
+import static org.ow2.chameleon.fuchsia.core.ImportationLinker.FILTER_IMPORTDECLARATION_PROPERTY;
+import static org.ow2.chameleon.fuchsia.core.ImportationLinker.FILTER_IMPORTERSERVICE_PROPERTY;
+
+@Configuration
+public class MqttLinkerInitializer {
+
+    Instance pushLinker = instance()
+            .of(FuchsiaConstants.DEFAULT_IMPORTATION_LINKER_FACTORY_NAME)
+            .named("MQTTLinker")
+            .with(FILTER_IMPORTDECLARATION_PROPERTY).setto("(id=*)")
+            .with(FILTER_IMPORTERSERVICE_PROPERTY).setto("(instance.name=AMQPImporter)");
+
+    Instance pushSubscriber = instance()
+            .of("AMQPImporterFactory")
+            .named("AMQPImporter")
+            .with("target").setto("(id=*)");
+
+}
