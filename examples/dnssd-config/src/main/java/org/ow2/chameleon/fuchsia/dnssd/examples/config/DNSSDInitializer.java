9d738245169aa8c153cf5dd0ea03bd86dc4c05b1
jbotnascimento <jbotnascimento@gmail.com>
2013-10-08 15:19:10 +0200

    add dnssd config example

---
+++ examples/dnssd-config/src/main/java/org/ow2/chameleon/fuchsia/dnssd/examples/config/DNSSDInitializer.java
@@ -0,0 +1,19 @@
+package org.ow2.chameleon.fuchsia.dnssd.examples.config;
+
+import org.apache.felix.ipojo.configuration.Configuration;
+import org.apache.felix.ipojo.configuration.Instance;
+import org.ow2.chameleon.fuchsia.core.FuchsiaConstants;
+
+import static org.apache.felix.ipojo.configuration.Instance.instance;
+import static org.ow2.chameleon.fuchsia.core.ImportationLinker.*;
+
+@Configuration
+public class DNSSDInitializer {
+
+    //This configuration starts a discovery to detect all local printer based on DNSSD/mDNS protocol
+    Instance dnssdDiscovery = instance()
+            .of("DNSSDDiscoveryFactory")
+            .named("DNSSDDiscovery")
+            .with("dnssd.service.type").setto("_printer._tcp.local.");
+
+}
