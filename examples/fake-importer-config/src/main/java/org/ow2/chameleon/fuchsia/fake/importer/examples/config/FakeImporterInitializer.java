e22d2b112fff85d3d50203f1ad4aac048992b8f8
jbotnascimento <jbotnascimento@gmail.com>
2013-12-04 13:32:59 +0100

    Fix example parenting (bad version reference)

---
+++ examples/fake-importer-config/src/main/java/org/ow2/chameleon/fuchsia/fake/importer/examples/config/FakeImporterInitializer.java
@@ -0,0 +1,18 @@
+package org.ow2.chameleon.fuchsia.fake.importer.examples.config;
+
+import org.apache.felix.ipojo.configuration.Configuration;
+import org.apache.felix.ipojo.configuration.Instance;
+import org.ow2.chameleon.fuchsia.core.FuchsiaConstants;
+
+import static org.apache.felix.ipojo.configuration.Instance.instance;
+import static org.ow2.chameleon.fuchsia.core.ImportationLinker.*;
+
+@Configuration
+public class FakeImporterInitializer {
+
+    Instance fakeImporterLinker = instance()
+            .of(FuchsiaConstants.DEFAULT_IMPORTATION_LINKER_FACTORY_NAME)
+            .named("DefaultLinkerFakeImporter")
+            .with(FILTER_IMPORTDECLARATION_PROPERTY).setto("(id=*)")
+            .with(FILTER_IMPORTERSERVICE_PROPERTY).setto("(instance.name=Fuchsia-FakeImporter)");
+}
