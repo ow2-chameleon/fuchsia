36dee7ebab17ac813f4c7608fa767325a550a4b6
jbotnascimento <jbotnascimento@gmail.com>
2014-02-10 13:36:50 +0100

    add help for philips hue command line

--- importers/philips-hue/src/main/java/org/ow2/chameleon/fuchsia/philips/hue/importer/PhilipsHueGogoCommand.java
+++ importers/philips-hue/src/main/java/org/ow2/chameleon/fuchsia/philips/hue/importer/PhilipsHueGogoCommand.java
@@ -25,8 +25,6 @@ public class PhilipsHueGogoCommand {
     @Requires(specification = "com.philips.lighting.model.PHLight")
     Set<PHLight> lights;
 
-    private static final int MAX_HUE=65535;
-
     @Requires
     PHBridge bridge;
 
@@ -36,7 +34,7 @@ public class PhilipsHueGogoCommand {
         this.m_context = context;
     }
 
-    @Descriptor  (value = "phlist")
+    @Descriptor  (value = "List all lights available")
     public void phlist(@Descriptor("phlist") String... parameters) {
         for(PHLight light:lights){
             System.out.println(light.toString());
@@ -44,10 +42,10 @@ public class PhilipsHueGogoCommand {
         }
     }
 
-    @Descriptor  (value = "phset")
-    public void phset(@Descriptor("phset") String... parameters) {
+    @Descriptor  (value = "Change light parameters, for a specific lamp or for all light plugged into the bridge")
+    public void phset(@Descriptor("[-name NAME] [-on true|false] [-i 0<=INTENSITY<=255] [-tt TRANSITION_TIME_IN_CENTISECONDS] [-r 0<=RED<=255] [-g 0<=GREEN<=255] [-b 0<=BLUE<=255]") String... parameters) {
 
-        String valueStr=getArgumentValue("-value",parameters);
+        String valueStr=getArgumentValue("-on",parameters);
         String nameStr=getArgumentValue("-name",parameters);
         String ttStr=getArgumentValue("-tt",parameters);
         String iStr=getArgumentValue("-i",parameters);
@@ -83,17 +81,13 @@ public class PhilipsHueGogoCommand {
                     int g=gStr!=null?Integer.valueOf(gStr):0;
                     int b=bStr!=null?Integer.valueOf(bStr):0;
 
-                    System.out.println(String.format("color %s %s %s",r,g,b));
+                    System.out.println(String.format("color red:%s green:%s blue:%s",r,g,b));
 
-                    System.out.println("model:"+light.getModelNumber());
+                    lightState.setColorMode(PHLight.PHLightColorMode.COLORMODE_HUE_SATURATION);
 
                     float[] xy= PHUtilities.calculateXYFromRGB(r, g, b, light.getIdentifier());
 
                     lightState.setHue(PHUtilities.colorFromXY(xy,light.getIdentifier()));
-                    lightState.setColorMode(PHLight.PHLightColorMode.COLORMODE_HUE_SATURATION);
-
-                    Random rand = new Random();
-                    lightState.setHue(rand.nextInt(MAX_HUE));
 
                 }
 
@@ -104,6 +98,12 @@ public class PhilipsHueGogoCommand {
         }
     }
 
+    /**
+     * Look up for the value of a parameter in a sequence of string
+     * @param option the option that we are looking for. e.g. '-k'
+     * @param params the list of parameters
+     * @return the value associated with the parameter or Null in case it is not present
+     */
     private String getArgumentValue(String option, String... params) {
 
         boolean found = false;
