312bea5cd3d5feda2cd7486a963787ef2f613a62
Morgan Martinet <morgan.martinet@imag.fr>
2014-02-12 16:58:44 +0100

    Move @Config from protobuffer exporter and importer to proto-config in examples

--- examples/proto-config/src/main/java/org/ow2/chameleon/fuchsia/protobuffer/protoconfig/Config.java
+++ examples/proto-config/src/main/java/org/ow2/chameleon/fuchsia/protobuffer/protoconfig/Config.java
@@ -1,14 +1,25 @@
-package org.ow2.chameleon.fuchsia.importer.protobuffer.testing;
+package org.ow2.chameleon.fuchsia.protobuffer.protoconfig;
 
 import org.apache.felix.ipojo.configuration.Configuration;
 import org.apache.felix.ipojo.configuration.Instance;
+import org.ow2.chameleon.fuchsia.core.ExportationLinker;
 import org.ow2.chameleon.fuchsia.core.FuchsiaConstants;
 import org.ow2.chameleon.fuchsia.core.ImportationLinker;
 
 import static org.apache.felix.ipojo.configuration.Instance.instance;
 
 @Configuration
-public class ProtobufferImporterConfig {
+public class Config {
+
+    Instance ProtobufferExporter = instance()
+            .of("org.ow2.chameleon.fuchsia.exporter.protobuffer.ProtobufferExporter")
+            .with("target").setto("(rpc.export.address=*)");
+
+    Instance ProtobufferExporterLinker = instance()
+            .of(FuchsiaConstants.DEFAULT_EXPORTATION_LINKER_FACTORY_NAME)
+            .with(ExportationLinker.FILTER_EXPORTDECLARATION_PROPERTY).setto("(rpc.export.address=*)")
+            .with(ExportationLinker.FILTER_EXPORTERSERVICE_PROPERTY).setto("(instance.name=ProtobufferExporter)");
+
 
     Instance ProtobufferRPCImporter = instance()
             .of("org.ow2.chameleon.fuchsia.importer.protobuffer.ProtobufferImporter")
@@ -20,4 +31,6 @@ public class ProtobufferImporterConfig {
             .with(ImportationLinker.FILTER_IMPORTDECLARATION_PROPERTY).setto("(&(rpc.server.address=*)(rpc.proto.class=*)(rpc.proto.service=*)(rpc.proto.message=*))")
             .with(ImportationLinker.FILTER_IMPORTERSERVICE_PROPERTY).setto("(instance.name=ProtobufferRPCImporter)");
 
+
 }
+
