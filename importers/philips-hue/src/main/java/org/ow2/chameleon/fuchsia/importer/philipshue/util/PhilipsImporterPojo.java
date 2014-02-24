2dbe63b9f4b2327cb3cd5210cb726c44be79ff80
jbotnascimento <jbotnascimento@gmail.com>
2014-02-24 10:24:59 +0100

    Throw proper exception in PhilipsHueImporter
    In case of not enough information provided for the importer, the right exception should be thrown

--- importers/philips-hue/src/main/java/org/ow2/chameleon/fuchsia/importer/philipshue/util/PhilipsImporterPojo.java
+++ importers/philips-hue/src/main/java/org/ow2/chameleon/fuchsia/importer/philipshue/util/PhilipsImporterPojo.java
@@ -1,6 +1,7 @@
 package org.ow2.chameleon.fuchsia.importer.philipshue.util;
 
 import org.ow2.chameleon.fuchsia.core.declaration.ImportDeclaration;
+import org.ow2.chameleon.fuchsia.core.exceptions.BinderException;
 
 public class PhilipsImporterPojo {
 
@@ -13,16 +14,20 @@ public class PhilipsImporterPojo {
 
     private PhilipsImporterPojo(){}
 
-    public static PhilipsImporterPojo create(ImportDeclaration importDeclaration){
+    public static PhilipsImporterPojo create(ImportDeclaration importDeclaration) throws BinderException {
 
         PhilipsImporterPojo dto=new PhilipsImporterPojo();
 
-        dto.id=importDeclaration.getMetadata().get("id").toString();
-        dto.name=importDeclaration.getMetadata().get("discovery.philips.device.name").toString();
-        dto.type=importDeclaration.getMetadata().get("discovery.philips.device.type").toString();
-        dto.object=importDeclaration.getMetadata().get("discovery.philips.device.object");
-        dto.bridgeType=importDeclaration.getMetadata().get("discovery.philips.bridge.type").toString();
-        dto.bridgeObject=importDeclaration.getMetadata().get("discovery.philips.bridge.object");
+        try{
+            dto.id=importDeclaration.getMetadata().get("id").toString();
+            dto.name=importDeclaration.getMetadata().get("discovery.philips.device.name").toString();
+            dto.type=importDeclaration.getMetadata().get("discovery.philips.device.type").toString();
+            dto.object=importDeclaration.getMetadata().get("discovery.philips.device.object");
+            dto.bridgeType=importDeclaration.getMetadata().get("discovery.philips.bridge.type").toString();
+            dto.bridgeObject=importDeclaration.getMetadata().get("discovery.philips.bridge.object");
+        }catch(NullPointerException npe){
+            throw new BinderException(npe);
+        }
 
         return dto;
 
