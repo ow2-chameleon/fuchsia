3558ba469532b9fc581a60e1c1d84785f1d8baae
jbotnascimento <jbotnascimento@gmail.com>
2014-02-25 14:49:27 +0100

    Protobuffer exporter bug fix
    Fix issue:
    -non available info during processing declaration
    -non shutting down endpoint in case of service stop

--- exporters/protobuffer/src/main/java/org/ow2/chameleon/fuchsia/exporter/protobuffer/ProtobufferExporter.java
+++ exporters/protobuffer/src/main/java/org/ow2/chameleon/fuchsia/exporter/protobuffer/ProtobufferExporter.java
@@ -6,21 +6,30 @@ import com.google.protobuf.Service;
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.binding.BindingFactoryManager;
+//import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
+import org.apache.cxf.transport.servlet.CXFServlet;
 import org.apache.felix.ipojo.annotations.*;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.component.Container;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
 import org.ow2.chameleon.fuchsia.core.FuchsiaUtils;
 import org.ow2.chameleon.fuchsia.core.component.AbstractExporterComponent;
 import org.ow2.chameleon.fuchsia.core.component.ExporterService;
 import org.ow2.chameleon.fuchsia.core.declaration.ExportDeclaration;
+import org.ow2.chameleon.fuchsia.core.exceptions.BinderException;
 import org.ow2.chameleon.fuchsia.exporter.protobuffer.internal.ProtobufferExporterPojo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,6 +41,13 @@ public class ProtobufferExporter extends AbstractExporterComponent {
     private static final Logger LOG = LoggerFactory.getLogger(ProtobufferExporter.class);
 
     private Map<String,Server> serverPublished=new HashMap<String,Server>();
+
+    org.eclipse.jetty.server.Server httpServer;
+
+    @ServiceProperty(name = "org.osgi.service.http.port")
+    private Integer HTTP_PORT;
+
+    private Bus cxfbus;
 
     @Requires
     HttpService http;
@@ -49,15 +65,71 @@ public class ProtobufferExporter extends AbstractExporterComponent {
     public void start() {
         System.setProperty("org.apache.cxf.nofastinfoset", "true");
         super.start();
+
+        CXFNonSpringServlet cxfServlet = new CXFNonSpringServlet();
+
+        try {
+
+            if(http!=null){
+                http.registerServlet("/cxf", cxfServlet, null, null);
+            }else {
+
+                try {
+
+                    cxfServlet=configStandaloneServer();
+
+                    httpServer.start();
+
+                } catch (Exception e1) {
+                    LOG.error("Impossible to start standalone CXF Jetty server.",e1);
+                }
+
+
+
+            }
+
+        } catch (ServletException e) {
+            LOG.error("Failed registering CXF servlet", e);
+        } catch (NamespaceException e) {
+            LOG.error("Failed registering CXF servlet", e);
+        }
+
+        cxfbus = cxfServlet.getBus();
+
+    }
+
+    private CXFServlet configStandaloneServer(){
+        httpServer = new org.eclipse.jetty.server.Server(HTTP_PORT);
+        Bus bus = BusFactory.getDefaultBus(true);
+        ContextHandlerCollection contexts = new ContextHandlerCollection();
+        httpServer.setHandler(contexts);
+        ServletContextHandler root = new ServletContextHandler(contexts, "/",
+                ServletContextHandler.SESSIONS);
+        CXFServlet cxf = new CXFServlet();
+        cxf.setBus(bus);
+        ServletHolder servlet = new ServletHolder(cxf);
+        root.addServlet(servlet, "/cxf/*");
+        return cxf;
     }
 
     @Invalidate
     public void stop(){
         super.stop();
+        if(httpServer!=null){
+            try {
+                httpServer.stop();
+            } catch (Exception e) {
+                LOG.warn("Failed to stop standalone server.",e);
+            }
+        }
+        for(Map.Entry<String,Server> entry:serverPublished.entrySet()){
+            serverPublished.remove(entry.getKey());
+            entry.getValue().stop();
+        }
     }
 
     @Override
-    protected void useExportDeclaration(ExportDeclaration exportDeclaration) {
+    protected void useExportDeclaration(ExportDeclaration exportDeclaration) throws BinderException {
 
         LOG.info("initiating exportation...");
 
@@ -65,13 +137,11 @@ public class ProtobufferExporter extends AbstractExporterComponent {
 
         try {
 
-            Class inter = FuchsiaUtils.loadClass(context, pojo.getClazz());
-            Class messageClass = FuchsiaUtils.loadClass(context, pojo.getMessage());
-
+            Class inter = FuchsiaUtils.loadClass(context, String.format("%s$%s", pojo.getClazz(), pojo.getService()));
+            Class messageClass = FuchsiaUtils.loadClass(context, String.format("%s$%s", pojo.getClazz(), pojo.getMessage()));
+            LOG.info("Looking for service that provides class {}",pojo.getClazz()+"$"+pojo.getService());
             Collection<ServiceReference<Service>> protobuffReferences = context.getServiceReferences(inter, pojo.getFilter());
-
             LOG.info("using filter " + pojo.getFilter() + " to find instance");
-
             if (protobuffReferences.size() == 0) {
 
                 LOG.info("nothing to be exported was found");
@@ -79,37 +149,20 @@ public class ProtobufferExporter extends AbstractExporterComponent {
             } else if (protobuffReferences.size() == 1) {
 
                 for (ServiceReference<Service> sr : protobuffReferences) {
-
                     Service protobufferService = context.getService(sr);
-
-                    Bus cxfbus = BusFactory.getThreadDefaultBus();
                     BindingFactoryManager mgr = cxfbus.getExtension(BindingFactoryManager.class);
                     mgr.registerBindingFactory(ProtobufBindingFactory.PROTOBUF_BINDING_ID, new ProtobufBindingFactory(cxfbus));
-
                     ProtobufServerFactoryBean serverFactoryBean = new ProtobufServerFactoryBean();
-
-                    //serverFactoryBean.setAddress("http://localhost:8889/AddressBookService");
-                    //serverFactoryBean.setAddress("http://localhost:8889/cxf/AddressBookService/");
-                    //serverFactoryBean.setServiceBean(new AddressBookProtos.AddressBookServiceImpl());
-
                     serverFactoryBean.setAddress(pojo.getAddress());
-
+                    serverFactoryBean.setBus(cxfbus);
                     serverFactoryBean.setServiceBean(inter.cast(protobufferService));
-
                     serverFactoryBean.setMessageClass(messageClass);
-
                     ClassLoader loader = Thread.currentThread().getContextClassLoader();
-
                     Thread.currentThread().setContextClassLoader(Container.class.getClassLoader());
-
                     Server server=serverFactoryBean.create();
-
                     serverPublished.put(pojo.getId(),server);
-
                     Thread.currentThread().setContextClassLoader(loader);
-
                     LOG.info("exporting the service with the id:" + pojo.getId());
-
                 }
 
             } else if (protobuffReferences.size() > 1) {
@@ -125,7 +178,7 @@ public class ProtobufferExporter extends AbstractExporterComponent {
     }
 
     @Override
-    protected void denyExportDeclaration(ExportDeclaration exportDeclaration) {
+    protected void denyExportDeclaration(ExportDeclaration exportDeclaration) throws BinderException {
 
         ProtobufferExporterPojo pojo=ProtobufferExporterPojo.create(exportDeclaration);
 
--- exporters/protobuffer/src/main/java/org/ow2/chameleon/fuchsia/exporter/protobuffer/internal/ProtobufferExporterPojo.java
+++ exporters/protobuffer/src/main/java/org/ow2/chameleon/fuchsia/exporter/protobuffer/internal/ProtobufferExporterPojo.java
@@ -1,6 +1,7 @@
 package org.ow2.chameleon.fuchsia.exporter.protobuffer.internal;
 
 import org.ow2.chameleon.fuchsia.core.declaration.ExportDeclaration;
+import org.ow2.chameleon.fuchsia.core.exceptions.BinderException;
 
 public class ProtobufferExporterPojo {
 
@@ -9,18 +10,31 @@ public class ProtobufferExporterPojo {
     String clazz;
     String message;
     String filter;
+    String service;
 
 
     private ProtobufferExporterPojo(){}
 
-    public static ProtobufferExporterPojo create(ExportDeclaration exportDeclaration){
+    public static ProtobufferExporterPojo create(ExportDeclaration exportDeclaration) throws BinderException{
 
         ProtobufferExporterPojo declaration=new ProtobufferExporterPojo();
 
-        declaration.id = exportDeclaration.getMetadata().get("id").toString();
-        declaration.address = exportDeclaration.getMetadata().get("rpc.export.address").toString();
-        declaration.clazz = exportDeclaration.getMetadata().get("rpc.export.class").toString();
-        declaration.message = exportDeclaration.getMetadata().get("rpc.export.message").toString();
+        try {
+
+            declaration.id = exportDeclaration.getMetadata().get("id").toString();
+            declaration.address = exportDeclaration.getMetadata().get("rpc.export.address").toString();
+            declaration.clazz = exportDeclaration.getMetadata().get("rpc.export.class").toString();
+            declaration.message = exportDeclaration.getMetadata().get("rpc.export.message").toString();
+            declaration.service = exportDeclaration.getMetadata().get("rpc.export.service").toString();
+
+        }catch(NullPointerException npe){
+
+            throw new BinderException(npe);
+
+        }
+
+
+
 
         Object filterRaw=exportDeclaration.getMetadata().get("rpc.export.filter");
 
@@ -53,4 +67,6 @@ public class ProtobufferExporterPojo {
     public String getFilter() {
         return filter;
     }
+
+    public String getService() {  return service; }
 }
