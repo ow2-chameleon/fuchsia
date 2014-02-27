5007093ac6e6c77fee5d1ab1e0295923106a822d
Morgan Martinet <morgan.martinet@imag.fr>
2014-02-27 13:21:19 +0100

    Fix sonar issues in proxies-utils

--- tools/proxies-utils/src/main/java/org/ow2/chameleon/fuchsia/tools/proxiesutils/ProxyFacetIntrospectable.java
+++ tools/proxies-utils/src/main/java/org/ow2/chameleon/fuchsia/tools/proxiesutils/ProxyFacetIntrospectable.java
@@ -1,5 +1,5 @@
 package org.ow2.chameleon.fuchsia.tools.proxiesutils;
 
 public interface ProxyFacetIntrospectable extends FuchsiaProxy {
-    public <T> T get(String varName, Class<T> klass);
+    <T> T get(String varName, Class<T> klass);
 }
