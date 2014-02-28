3e77162a3641f51b9053bd62c4e17cf13e614076
jbotnascimento <jbotnascimento@gmail.com>
2014-02-28 16:25:02 +0100

    sonar fix: add private constructor for class containing constants

--- discoveries/mdns/src/main/java/org/ow2/chameleon/fuchsia/discovery/mdns/MDNSConstants.java
+++ discoveries/mdns/src/main/java/org/ow2/chameleon/fuchsia/discovery/mdns/MDNSConstants.java
@@ -1,8 +1,9 @@
 package org.ow2.chameleon.fuchsia.discovery.mdns;
 
-
-public class MDNSConstants {
+public final class MDNSConstants {
 
     public static final String DNSSD_SERVICE_TYPE ="_http._tcp.local.";
+
+    private MDNSConstants(){}
 
 }
--- exporters/jax-ws/src/main/java/org/ow2/chameleon/fuchsia/exporter/jaxws/internal/Constants.java
+++ exporters/jax-ws/src/main/java/org/ow2/chameleon/fuchsia/exporter/jaxws/internal/Constants.java
@@ -1,10 +1,12 @@
 package org.ow2.chameleon.fuchsia.exporter.jaxws.internal;
 
-public class Constants {
+public final class Constants {
 
     public static final String CXF_SERVLET="/cxf";
     public static final String CXF_EXPORT_FILTER ="fuchsia.export.cxf.filter";
     public static final String CXF_EXPORT_TYPE="fuchsia.export.cxf.class.name";
     public static final String CXF_EXPORT_WEB_CONTEXT="fuchsia.export.cxf.url.context";
+
+    private Constants(){}
 
 }
